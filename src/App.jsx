29d447b182373d48178d277081030bc5cd4b2818
motty mandel <mandelmottyisrael65@gmail.com>
2025-05-05 00:00:47 -0400

    added a welcome wavy sign

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+import './index.css';
+
+function App() {
+    return (
+        <div className='app-content'>
+
+              <Outlet />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './css/Footer.css';
+
+export default function Footer() {
+    return (
+        <p>hello</p>
+    )
+}
